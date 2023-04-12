ebefe87fd6ea92d78e322dd57b07b4f4f2db1072
yurilopesm <yurilopesm@hotmail.com>
2023-04-12 12:21:50 -0300

    requisito armazenamento de dados finalizados

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express"
 import cors from 'cors'
+import { MongoClient } from "mongodb"
+import dotenv from 'dotenv'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+dotenv.config()
+
+//Conexão com o Banco
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => res.status(500).send(err.message))
+
 
 
 const PORT = 5000
